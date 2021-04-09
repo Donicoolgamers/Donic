9da2b7a54b0b45e9d3995a53878def694a9f2338
D O <driesolbrechts@gmail.com>
2021-04-09 14:38:20 +0200

    joystick werkt nu

--- donic/donic.ino
+++ donic/donic.ino
@@ -7,18 +7,29 @@
 //define this if you dont want to use ULTRASONE sensor
 //#define ULTRASONE
 
+//define this if you just want to test with the joystick
+//#define JOYSTICK
 
 
 #include "ultrasonic.h"
 #include "screens.h"
 #include "buzzer.h"
+#include "joystick.h"
 
 // Pins
 #define ECHO 11
 #define TRIG 12
 
+#define JOYX A0
+#define JOYY A1
+#define JOYSW 2
+
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 Screen screen(&lcd);
+
+Joystick joystick(JOYSW, JOYX , JOYY);
 
 Ultrasonic sonic(ECHO, TRIG);
 
@@ -26,7 +37,9 @@ long time;
 int distance;
 
 void setup() {
+    
     screen.init();
+    joystick.init();
     screen.drawWelcome();
 
     // Loading
@@ -41,9 +54,14 @@ void setup() {
     #endif
 
     Serial.begin(9600);
+    
 
     #ifdef ULTRASONE
     Serial.println("--------------------DEBUG MODE : no ultrasone sensor--------------------");
+    #endif
+
+    #ifdef JOYSTICK
+    Serial.println("--------------------DEBUG MODE : joystick debugging--------------------");
     #endif
 
     delay(1000);
@@ -61,11 +79,28 @@ void loop() {
     distance = distanceInput.toInt();
     #endif
     
+    #ifndef JOYSTICK
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.print(" cm\n");
+    #endif
+
+    #ifdef JOYSTICK
+    joystick.readValues();
+    
+    Serial.print("x: ");
+    Serial.print(joystick.getX());
+    Serial.print("\n");
+    Serial.print("y: ");
+    Serial.print(joystick.getY());
+    Serial.print("\n");
+    Serial.print("pressed: ");
+    Serial.print(joystick.getPressed());
+    Serial.print("\n");
+    #endif
 
     screen.drawDistance(distance);
+
 
     delay(1000);
 }
